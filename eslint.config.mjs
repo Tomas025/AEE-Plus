e0bc5a0131233d858e54e756f671b9dada98fd0f
Tomas025 <tomasbraz43@hotmail.com>
2025-07-13 00:02:20 -0300

    build: configurado ambiente de desenvolvimento

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,41 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:prettier/recommended",
+      "prettier",
+    ],
+    plugins: ["eslint-plugin-import-helpers"],
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+      "import-helpers/order-imports": [
+        "warn",
+        {
+          newlinesBetween: "always",
+          groups: [
+            ["/^react/", "/^next/", "/@next/"],
+            "/components/",
+            "module",
+            "/^@shared/",
+            "/absolute/",
+            ["parent", "sibling", "index"],
+          ],
+          alphabetize: { order: "asc", ignoreCase: true },
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
